51e17fb4eaab1835fb7855bdf7588690a7f606e4
guzhikova <guzhikova.a@gmail.com>
2020-02-27 19:43:09 +0400

    Logger

---
+++ GuzhikovaAA.DentalOffice/DentalOffice.Log4net/Logger.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalOffice.Log4net
+{
+    public class Logger
+    {
+        private static ILog _log = LogManager.GetLogger("LOGGER");
+
+        public static ILog Log
+        {
+            get { return _log; }
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
