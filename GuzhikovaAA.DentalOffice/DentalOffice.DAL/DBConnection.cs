6832f3bf24655d075ae32daf76627d6f31950ef0
guzhikova <guzhikova.a@gmail.com>
2020-03-03 20:17:16 +0400

    db

---
+++ GuzhikovaAA.DentalOffice/DentalOffice.DAL/DBConnection.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalOffice.DAL
+{
+    internal class DBConnection
+    {
+        public static string ConnectionString => 
+            @"Data Source=ANASTASIA\SQLEXPRESS;Initial Catalog=UsersAndAwards;Integrated Security=True";
+
+        /// <summary>
+        /// Executes stored procedure with input parameters.
+        /// </summary>
+        /// <param name="name">Name of stored procedure </param>
+        /// <param name="parameters">Transmitted input SQL-parameters</param>
+        /// <returns>Number of processed rows</returns>
+        public int ExecuteStoredProcedure(string name, SqlParameter[] parameters)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.CommandText = name;
+
+                command.Parameters.AddRange(parameters);
+
+                connection.Open();
+               return command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Executes stored procedure with one input parameter.
+        /// </summary>
+        /// <param name="name">Name of stored procedure </param>
+        /// <param name="parameters">Transmitted input SQL-parameter (usually ID)</param>
+        /// <returns>Number of processed rows</returns>
+        public int ExecuteStoredProcedure(string name, SqlParameter parameter)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.CommandText = name;
+
+                command.Parameters.Add(parameter);
+
+                connection.Open();
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Executes stored procedure with input parameters and one output parameter.
+        /// </summary>
+        /// <param name="name">Name of stored procedure </param>
+        /// <param name="output">Output SQL-parameter name</param>
+        /// <param name="parameters">Transmitted input SQL-parameters</param>
+        /// <returns>Return value of output SQL-parameter. </returns>
+        public object ExecuteStoredProcedure(string name, SqlParameter[] inputParameters, SqlParameter outputParameter)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                var command = connection.CreateCommand();
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.CommandText = name;
+
+                command.Parameters.AddRange(inputParameters);
+                command.Parameters.Add(outputParameter);
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+
+                return  outputParameter.Value;
+            }
+        }
+
+    }
+}
--- GuzhikovaAA.DentalOffice/DentalOffice.DAL/UsersDao.cs
+++ GuzhikovaAA.DentalOffice/DentalOffice.DAL/UsersDao.cs
@@ -2,6 +2,7 @@
 using DentalOffice.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,42 @@ namespace DentalOffice.DAL
 {
     public class UsersDao : IUsersDao
     {
+        DBConnection _dbConnection = new DBConnection();
+
         public User Add(User user)
         {
-            throw new NotImplementedException();
+    
+                var idParameter = new SqlParameter()
+                {
+                    DbType = System.Data.DbType.Int32,
+                    ParameterName = "@id",
+                    Direction = System.Data.ParameterDirection.Output
+                };
+
+               /////....................
+
+            SqlParameter[] parameters =
+            {
+               //........................
+            };
+
+            user.ID = (int)_dbConnection.ExecuteStoredProcedure("dbo.AddUser", parameters, idParameter);
+
+            return user;
+            
         }
 
         public void DeleteById(int id)
         {
-            throw new NotImplementedException();
+            var idParameter = new SqlParameter()
+            {
+                DbType = System.Data.DbType.Int32,
+                ParameterName = "@id",
+                Value = id,
+                Direction = System.Data.ParameterDirection.Input
+            };
+
+            _dbConnection.ExecuteStoredProcedure("dbo.DeleteUserById", idParameter);
         }
 
         public IEnumerable<User> GetAll()
@@ -32,7 +61,25 @@ namespace DentalOffice.DAL
 
         public User Update(User user)
         {
-            throw new NotImplementedException();
+
+            var idParameter = new SqlParameter()
+            {
+                DbType = System.Data.DbType.Int32,
+                ParameterName = "@id",
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            /////....................
+
+            SqlParameter[] parameters =
+            {
+               //........................
+            };
+
+
+            _dbConnection.ExecuteStoredProcedure("dbo.UpdateUser", parameters);
+            
+            return user;
         }
     }
 }
